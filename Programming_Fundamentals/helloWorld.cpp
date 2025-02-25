6ec7698362fc050a5702241d2f3d71326c742ff3
Parth2684 <bhosle6006@gmail.com>
2025-02-25 20:38:37 +0530

    first code

---
+++ Programming_Fundamentals/helloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Namaste Bharat" << endl ;
+    cout << "Enter a number: " << endl;
+
+    int a;
+    cin >> a;
+
+    cout << "You entered" << a << endl; 
+}
